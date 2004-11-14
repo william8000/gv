41871087ba470008d71870cac07e9a9665adead7
Jose E. Marchesi <jemarch@gnu.org>
2004-11-14 01:16:12 +0000

    Solved bug  #10971. Task  #3562 done

--- gv/src/resource.c
+++ gv/src/resource.c
@@ -33,7 +33,7 @@
 #define _GV_RESOURCE_C_
 
 /*
-#define MESSAGES
+  #define MESSAGES
 */
 #include "message.h"
 
@@ -59,32 +59,32 @@
 
 static String class_resources[] = {
 #   include "gv_class.h"
-    NULL
+  NULL
 };
 
 String intern_resources[] = {
 #   include INTERN_RESOURCES_H
-    NULL
+  NULL
 };
 
 #ifdef USE_FALLBACK_STYLES
 #  ifdef FALLBACK_STYLE_1_H
-     static String fallback_style_1[] = {
+static String fallback_style_1[] = {
 #      include FALLBACK_STYLE_1_H
-       NULL
-     };
+  NULL
+};
 #  endif
 #  ifdef FALLBACK_STYLE_2_H
-     static String fallback_style_2[] = {
+static String fallback_style_2[] = {
 #      include FALLBACK_STYLE_2_H
-       NULL
-     };
+  NULL
+};
 #  endif
 #  ifdef FALLBACK_STYLE_3_H
-     static String fallback_style_3[] = {
+static String fallback_style_3[] = {
 #      include FALLBACK_STYLE_3_H
-       NULL
-     };
+  NULL
+};
 #  endif
 #endif
 
@@ -95,29 +95,29 @@ static String resource_ad_file     = NULL;
 static char* resource_mergeFileIntoDatabase();
 
 /*#######################################################
-   resource_freeData
-#######################################################*/
+  resource_freeData
+  #######################################################*/
 
 void resource_freeData()
 {
   BEGINMESSAGE(resource_freeData)
-  if (resource_system_file) GV_XtFree(resource_system_file);
+    if (resource_system_file) GV_XtFree(resource_system_file);
   if (resource_user_file)   GV_XtFree(resource_user_file);
   if (resource_style_file)  GV_XtFree(resource_style_file);
   if (resource_ad_file)     GV_XtFree(resource_ad_file);
   ENDMESSAGE(resource_freeData)
-}
+    }
 
 /*#######################################################
-   resource_buildDatabase
-#######################################################*/
+  resource_buildDatabase
+  #######################################################*/
 
 XrmDatabase resource_buildDatabase(display,app_class,app_name,argcP,argv)
-  Display *display;
-  char *app_class;
-  char *app_name;
-  int *argcP;
-  char **argv;
+     Display *display;
+     char *app_class;
+     char *app_name;
+     int *argcP;
+     char **argv;
 {
   XrmDatabase db=NULL;
   String *sP;
@@ -129,36 +129,38 @@ XrmDatabase resource_buildDatabase(display,app_class,app_name,argcP,argv)
 
   BEGINMESSAGE(resource_buildDatabase)
 
-  /* ### class resources ################# */
-  INFMESSAGE(merging class resources into database)
-  sP = class_resources;
+    /* ### class resources ################# */
+    INFMESSAGE(merging class resources into database)
+    sP = class_resources;
   while (*sP) XrmPutLineResource(&db,*sP++);
 
   /* ### system resources ################# */
   INFMESSAGE(checking for system resources)
-#ifdef VMS
-  b=0;
-  sprintf(tmp,"DECW$USER_DEFAULTS:%s_SYSTEM.DAT",app_class);
-  if (!file_fileIsNotUseful(tmp)) b=1;
-  if (!b) {
-    sprintf(tmp,"DECW$SYSTEM_DEFAULTS:%s_SYSTEM.DAT",app_class);
-    if (!file_fileIsNotUseful(tmp)) b=1;
-  }
-  if (b) s = GV_XtNewString(tmp);
-  else s = NULL;
-#else
-  s = XtResolvePathname(display,"app-defaults",NULL,NULL,NULL,NULL,0,NULL);
-#endif
-  if (s) {
-    INFSMESSAGE(merging system resource file into database,s)
-    XrmCombineFileDatabase(s,&db,True);
-    resource_system_file = s;
-  }
+    /* #ifdef VMS */
+    /*   b=0; */
+    /*   sprintf(tmp,"DECW$USER_DEFAULTS:%s_SYSTEM.DAT",app_class); */
+    /*   if (!file_fileIsNotUseful(tmp)) b=1; */
+    /*   if (!b) { */
+    /*     sprintf(tmp,"DECW$SYSTEM_DEFAULTS:%s_SYSTEM.DAT",app_class); */
+    /*     if (!file_fileIsNotUseful(tmp)) b=1; */
+    /*   } */
+    /*   if (b) s = GV_XtNewString(tmp); */
+    /*   else s = NULL; */
+    /* #else */
+    //  s = XtResolvePathname(display,"app-defaults",NULL,NULL,NULL,NULL,0,NULL);
+    /* #endif */
+
+    String rpath = GV_XtNewString(GV_LIBDIR);
+    if (rpath) {
+      INFSMESSAGE(merging system resource file into database,rpath)
+	XrmCombineFileDatabase(rpath,&db,True);
+      resource_system_file = rpath;
+    }
 
   /* ### user resources ################# */
   INFMESSAGE(checking for user resources)
 #ifdef VMS
-  sprintf(tmp,"DECW$USER_DEFAULTS:%s.DAT",app_class);
+    sprintf(tmp,"DECW$USER_DEFAULTS:%s.DAT",app_class);
   if (file_fileIsNotUseful(tmp)) s=NULL;
   else s = GV_XtNewString(tmp);
 #else
@@ -173,7 +175,7 @@ XrmDatabase resource_buildDatabase(display,app_class,app_name,argcP,argv)
 #endif
   if (s) {
     INFSMESSAGE(merging user resource file into database,s)
-    XrmCombineFileDatabase(s,&db,True);
+      XrmCombineFileDatabase(s,&db,True);
     resource_user_file = s;
   } else {
     resource_user_file = GV_XtNewString(tmp);
@@ -181,43 +183,43 @@ XrmDatabase resource_buildDatabase(display,app_class,app_name,argcP,argv)
 
   /* ### command line resources ################# */
   INFMESSAGE(parsing command line)
-  XrmParseCommand(&db,options,XtNumber(options),app_name,argcP,argv);
+    XrmParseCommand(&db,options,XtNumber(options),app_name,argcP,argv);
 
   /* ### style resources ######################## */
   INFMESSAGE(checking for style resources)
-  s = resource_getResource(db,app_class,app_name,"style","Style");
+    s = resource_getResource(db,app_class,app_name,"style","Style");
   if (s) {
-      t=NULL;
+    t=NULL;
 #   ifdef USE_FALLBACK_STYLES
-      sP = NULL;
+    sP = NULL;
 #     ifdef FALLBACK_STYLE_1_DAT
-        if (!strcmp(FALLBACK_STYLE_1_DAT,s)) sP = fallback_style_1;
+    if (!strcmp(FALLBACK_STYLE_1_DAT,s)) sP = fallback_style_1;
 #     endif
 #     ifdef FALLBACK_STYLE_2_DAT
-        if (!strcmp(FALLBACK_STYLE_2_DAT,s)) sP = fallback_style_2;
+    if (!strcmp(FALLBACK_STYLE_2_DAT,s)) sP = fallback_style_2;
 #     endif
 #     ifdef FALLBACK_STYLE_3_DAT
-        if (!strcmp(FALLBACK_STYLE_3_DAT,s)) sP = fallback_style_3;
+    if (!strcmp(FALLBACK_STYLE_3_DAT,s)) sP = fallback_style_3;
 #     endif
-      if (sP) {
-	INFMESSAGE(merging fallback style resources into database)
+    if (sP) {
+      INFMESSAGE(merging fallback style resources into database)
 	while (*sP) XrmPutLineResource(&db,*sP++);
-        s = NULL;
-      } else {
-	INFSMESSAGE(merging style resource file into database,s)
-        t = resource_mergeFileIntoDatabase(&db,s);
-	if (!t) {
-	  fprintf(stderr,"%s: Style file '%s' not found - ignored\n",app_name,s);
-	  s=NULL;
-	} else s = t;
-      }
-#   else
+      s = NULL;
+    } else {
       INFSMESSAGE(merging style resource file into database,s)
-      t = resource_mergeFileIntoDatabase(&db,s);
+        t = resource_mergeFileIntoDatabase(&db,s);
       if (!t) {
 	fprintf(stderr,"%s: Style file '%s' not found - ignored\n",app_name,s);
 	s=NULL;
       } else s = t;
+    }
+#   else
+    INFSMESSAGE(merging style resource file into database,s)
+      t = resource_mergeFileIntoDatabase(&db,s);
+    if (!t) {
+      fprintf(stderr,"%s: Style file '%s' not found - ignored\n",app_name,s);
+      s=NULL;
+    } else s = t;
 #   endif
     if (s) {
       s = GV_XtNewString(s);
@@ -228,10 +230,10 @@ XrmDatabase resource_buildDatabase(display,app_class,app_name,argcP,argv)
 
   /* ### ad resources ######################## */
   INFMESSAGE(checking for ad resources)
-  s = resource_getResource(db,app_class,app_name,"ad","Ad");
+    s = resource_getResource(db,app_class,app_name,"ad","Ad");
   if (s) {
     INFSMESSAGE(merging ad resource file into database,s)
-    t = resource_mergeFileIntoDatabase(&db,s);
+      t = resource_mergeFileIntoDatabase(&db,s);
     if (!t) {
       fprintf(stderr,"%s: ad file '%s' not found - ignored\n",app_name,s);
       s=NULL;
@@ -244,19 +246,19 @@ XrmDatabase resource_buildDatabase(display,app_class,app_name,argcP,argv)
   }
 
   ENDMESSAGE(resource_buildDatabase)
-  return(db);
+    return(db);
 }
 
 /*#######################################################
-    resource_getResource
-#######################################################*/
+  resource_getResource
+  #######################################################*/
 
 char *resource_getResource(db,app_class,app_name,name_val,class_val)
-  XrmDatabase db;
-  char *app_class;
-  char *app_name;
-  char *name_val;
-  char *class_val;
+     XrmDatabase db;
+     char *app_class;
+     char *app_name;
+     char *name_val;
+     char *class_val;
 {
   char *result=NULL;
   XrmValue rm_value;
@@ -265,20 +267,20 @@ char *resource_getResource(db,app_class,app_name,name_val,class_val)
   Bool success=False;
 
   BEGINMESSAGE(resource_getResource)
-  if (!class_val) class_val=name_val;
+    if (!class_val) class_val=name_val;
   sprintf(str_name, "%s.%s",app_name,name_val);
   sprintf(str_class,"%s.%s",app_class,class_val);
   INFSMESSAGE(looking for,str_class)
-  success = XrmGetResource(db,str_name,str_class,&str_type,&rm_value);
+    success = XrmGetResource(db,str_name,str_class,&str_type,&rm_value);
   if  (success && rm_value.size>1) { result = (char*) rm_value.addr; INFSMESSAGE(found entry,result) }
   else                             { result = NULL;                  INFMESSAGE(no entry found)      }
   ENDMESSAGE(resource_getResource)
-  return(result);
+    return(result);
 }
 
 /*#######################################################
-    resource_checkGeometryResource
-#######################################################*/
+  resource_checkGeometryResource
+  #######################################################*/
 
 #ifdef max
 #   undef max
@@ -286,9 +288,9 @@ char *resource_getResource(db,app_class,app_name,name_val,class_val)
 #define max(a,b) ((a)>(b)?(a):(b))
 
 int resource_checkGeometryResource(dbP,app_class,app_name)
-  XrmDatabase *dbP;
-  char *app_class;
-  char *app_name;
+     XrmDatabase *dbP;
+     char *app_class;
+     char *app_name;
 {
   char tmp[GV_MAX_FILENAME_LENGTH];
   char *pos;
@@ -299,15 +301,15 @@ int resource_checkGeometryResource(dbP,app_class,app_name)
   unsigned int gwidth=0,gheight=0;
 
   BEGINMESSAGE(resource_checkGeometryResource)
-  pos = resource_getResource(*dbP,app_class,app_name,"geometry","Geometry");
+    pos = resource_getResource(*dbP,app_class,app_name,"geometry","Geometry");
   if (!pos) {
     ENDMESSAGE(resource_checkGeometryResource)
-    return(dim_forced);
+      return(dim_forced);
   }
   INFSMESSAGE(found geometry resource,pos)
-  flag=XParseGeometry(pos,&gx,&gy,&gwidth,&gheight);
+    flag=XParseGeometry(pos,&gx,&gy,&gwidth,&gheight);
   IIMESSAGE(gx,gy) IIMESSAGE(gwidth,gheight)
-  if (gwidth)  gwidth   = max(gwidth,GV_MINIMUM_SIZE);
+    if (gwidth)  gwidth   = max(gwidth,GV_MINIMUM_SIZE);
   if (gheight) gheight  = max(gheight,GV_MINIMUM_SIZE);
   tmp[0]='\0';
   if (flag&WidthValue) {
@@ -327,31 +329,31 @@ int resource_checkGeometryResource(dbP,app_class,app_name)
   if (tmp[0]) {
     char s[50];
     INFSMESSAGE(corrected geometry,tmp)
-    sprintf(s,"%s.geometry",app_name);
+      sprintf(s,"%s.geometry",app_name);
     SMESSAGE(s) SMESSAGE(tmp)
-    XrmPutStringResource(dbP,s,tmp);
+      XrmPutStringResource(dbP,s,tmp);
 #   ifdef MESSAGES
     pos = resource_getResource(*dbP,app_class,app_name,"geometry","Geometry");
     if (pos) { INFSMESSAGE(check:,pos) }
 #   endif
   }
   ENDMESSAGE(resource_checkGeometryResource)
-  return(dim_forced);
+    return(dim_forced);
 }
 
 /*#######################################################
-    resource_checkResources
-#######################################################*/
+  resource_checkResources
+  #######################################################*/
 
 int resource_checkResources(app_name,v,vc)
-  char *app_name;
-  char *v;
-  char *vc;
+     char *app_name;
+     char *v;
+     char *vc;
 {
   int n,nc,v1,v2,v3,v1c,v2c,v3c,r=1;
 
   BEGINMESSAGE(resource_checkResources)
-  v1 = v2 = v3 = v1c = v2c = v3c = 0;
+    v1 = v2 = v3 = v1c = v2c = v3c = 0;
   n  = sscanf(v,"%*s %d.%d.%d",&v1,&v2,&v3);
   nc = sscanf(vc,"%*s %d.%d.%d",&v1c,&v2c,&v3c);
   if ((n < 2) || (nc < 2)
@@ -369,85 +371,85 @@ int resource_checkResources(app_name,v,vc)
     r=0;
   }
   ENDMESSAGE(resource_checkResources)
-  return(r);
+    return(r);
 }
 
 
 /*#######################################################
-    resource_userResourceFile()
-#######################################################*/
+  resource_userResourceFile()
+  #######################################################*/
 
 char *resource_userDefaultsFile()
 {
   char *s;
   BEGINMESSAGE(resource_userResourceFile)
-  if      (resource_ad_file)   s = resource_ad_file;
-  else if (resource_user_file) s = resource_user_file;
-  else                         s = USER_DEFAULTS;
+    if      (resource_ad_file)   s = resource_ad_file;
+    else if (resource_user_file) s = resource_user_file;
+    else                         s = USER_DEFAULTS;
   s = GV_XtNewString(s);
   return(s);
   ENDMESSAGE(resource_userResourceFile)  
-}
+    }
 
 
 /*---------------------------------------------------------------
-    resource_mergeFileIntoDatabase
----------------------------------------------------------------*/
+  resource_mergeFileIntoDatabase
+  ---------------------------------------------------------------*/
 
 static char* resource_mergeFileIntoDatabase(dbP,name)
-  XrmDatabase *dbP;
-  char *name;
+     XrmDatabase *dbP;
+     char *name;
 {
   char tmp[GV_MAX_FILENAME_LENGTH];
   int useful=0;
 
   BEGINMESSAGE(resource_mergeFileIntoDatabase)
 
-  if (!name) {
-    ENDMESSAGE(resource_mergeFileIntoDatabase)
-   return(name);
-  }
+    if (!name) {
+      ENDMESSAGE(resource_mergeFileIntoDatabase)
+	return(name);
+    }
 
   strcpy(tmp,name);
   file_translateTildeInPath(tmp);
 
   if (file_fileIsNotUseful(tmp)) {
     INFSMESSAGE(not useful,tmp)
-    if (name != file_locateFilename(name)) useful=-1;
-    else {
+      if (name != file_locateFilename(name)) useful=-1;
+      else {
 #     ifdef VMS
         sprintf(tmp,"DECW$USER_DEFAULTS:%s",name);
 #     else
 	sprintf(tmp,"~/%s",name);
 	file_translateTildeInPath(tmp);
 #     endif
-    }
+      }
   } else useful=1;
 
   if (!useful) {
 #   ifdef VMS
-      if (file_fileIsNotUseful(tmp)) {
-	INFSMESSAGE(not useful,tmp)
+    if (file_fileIsNotUseful(tmp)) {
+      INFSMESSAGE(not useful,tmp)
 	sprintf(tmp,"SYS$LOGIN:%s",name);
-      } else useful=1;     
+    } else useful=1;     
 #   endif
     if (!useful && file_fileIsNotUseful(tmp)) {
       INFSMESSAGE(not useful,tmp)
-      sprintf(tmp,"%s%s",GV_LIBDIR,name);
+	sprintf(tmp,"%s%s",GV_LIBDIR,name);
     } else useful=1;
 
     if (!useful && file_fileIsNotUseful(tmp)) {
       INFSMESSAGE(not useful,tmp)
-      INFMESSAGE(giving up)
-    } else useful=1;
+	INFMESSAGE(giving up)
+	} else useful=1;
   }
 
   if (useful==1) {
     INFSMESSAGE(merging,tmp)
-    XrmCombineFileDatabase(tmp,dbP,True);
+      XrmCombineFileDatabase(tmp,dbP,True);
     name = GV_XtNewString(tmp);
   }
   else name=NULL;
   ENDMESSAGE(resource_mergeFileIntoDatabase)
-  return(name);
+    return(name);
 }
