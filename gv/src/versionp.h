c456bae76bee6ecc7704d4c86611bae732cfec51
Markus Steinborn <gnugv_maintainer@yahoo.de>
2010-04-24 18:26:05 +0200

    Bumped version to 3.6.90

--- gv/src/versionp.h
+++ gv/src/versionp.h
@@ -40,7 +40,7 @@
 #define  VERSION_APPLIC_CONTEXT	app_con  			/* the Application Context */
 
 char *versionIdentification[] = {
-        "gv 3.6.9",
+        "gv 3.6.90",
         "April 2010",
 	0
 };
