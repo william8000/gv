6a110f7ef947c3777a266be87a1ba7c0d96f7de9
Markus Steinborn <gnugv_maintainer@yahoo.de>
2012-04-01 17:28:54 +0200

    Bump verson to 3.7.3.91

--- gv/src/versionp.h
+++ gv/src/versionp.h
@@ -40,8 +40,8 @@
 #define  VERSION_APPLIC_CONTEXT	app_con  			/* the Application Context */
 
 char *versionIdentification[] = {
-        "gv 3.7.3.90",
-        "March 2011",
+        "gv 3.7.3.91",
+        "April 2012",
 	0
 };
 
