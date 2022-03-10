f8e17bf076549aab623a8db8c57d00b1f9433414
William Bader <william@newspapersystems.com>
2022-03-10 00:05:00 +0000

    Add makedist.sh --no-dist to suppress making a distribution. Update makedist.sh to run from anywhere inside the repo. Update .gitignore to ignore 'make dist' results in gv/gv-* .

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -2,14 +2,19 @@
 # Passes command line options to configure.
 # Useful options:
 # CFLAGS=-O2 # optimize and no -g for distribution
+# --no-dist # first to suppress making distribution
 # --with-default-papersize=Letter # change default from A4 to Letter
 
-cd .. || exit
+dir=$(git rev-parse --show-toplevel)
+if [ -z "$dir" ] ; then echo "$0: Error: not inside gv git" ; fi
+cd "$dir" || exit
+if [ ! -d "gv" ] || [ ! -d "scripts" ] ; then echo "$0: Error: gv directories not found in $dir" ; fi
 git clean -dfx
 cd gv || exit
+if [ "$1" = "--no-dist" ] ; then dist="no" ; shift ; fi
 PATH=/opt/autotools/bin:${PATH} autoreconf -vi
 ./configure "$@"
 np=$(nproc)
 if [ -z "$np" ] ; then np=1 ; fi
 make -j "$np"
-make dist
+if [ "$dist" != "no" ] ; then make dist ; fi
